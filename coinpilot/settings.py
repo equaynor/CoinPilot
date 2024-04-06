75e8149f4ca3e39f5597d9bc187428c686a8e9f4
equaynor <e.c.quaynor@gmail.com>
2024-04-06 13:05:29 +0000

    Add user model
    - Delete previous migrations because of inconsistancies

--- coinpilot/settings.py
+++ coinpilot/settings.py
@@ -133,7 +133,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
