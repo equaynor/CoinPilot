d80a1fe2c6273e1c52ec09be526942c9d1e59c77
equaynor <e.c.quaynor@gmail.com>
2024-04-06 14:00:13 +0000

    Update base.html and add custom css

--- coinpilot/settings.py
+++ coinpilot/settings.py
@@ -32,10 +32,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-equaynor-coinpilot-e4xigmpgds.us1.codeanyapp.com',
+    '8000-equaynor-coinpilot-8vtbbp773f.us1.codeanyapp.com',
     '.herokuapp.com',]
 
 
