6e5ecc7ef9b88092f5b497c3abccba950006fb49
equaynor <e.c.quaynor@gmail.com>
2024-04-29 15:26:22 +0000

    Add sorting and filtering functionality to coin list

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from coin.models import Coin
-
-def get_coins():
-    coins = Coin.objects.all()
-    return coins
+from django.db.models import Q
 
 def coin_list(request):
-    coins = get_coins()  # Fetch the coin data from the database
+    query = request.GET.get('q')
+    sort_by = request.GET.get('sort_by')
+    coins = Coin.objects.all()
+
+    if query:
+        coins = coins.filter(
+            Q(name__icontains=query) | Q(symbol__icontains=query)
+        )
+
+    if sort_by == 'name':
+        coins = coins.order_by('name')
+    elif sort_by == 'price':
+        coins = coins.order_by('-current_price')
+    elif sort_by == 'market_cap':
+        coins = coins.order_by('-market_cap')
+    elif sort_by == 'change_24h':
+        coins = coins.order_by('-price_change_percentage_24h')
+
     context = {
-        'coins': coins
+        'coins': coins,
+        'query': query,
+        'sort_by': sort_by
     }
     return render(request, 'portfolio/coin_list.html', context)
