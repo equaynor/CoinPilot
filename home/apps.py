988c394732f41d98ca86759f4a63b9c0a54c8625
equaynor <e.c.quaynor@gmail.com>
2024-05-02 10:41:38 +0000

    Create home config to import signals

--- home/apps.py
+++ home/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+
+    def ready(self):
+        from . import signals  # Import the signals module to connect the handlers
