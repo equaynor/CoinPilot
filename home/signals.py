6ebdd6dec6d42bf8decab5cc1d7ab6990b17c968
equaynor <e.c.quaynor@gmail.com>
2024-05-02 10:40:23 +0000

    Create signals.py to fetch coin data flag

---
+++ home/signals.py
@@ -0,0 +1,20 @@
+from django.dispatch import receiver
+from allauth.account.signals import user_logged_in, user_logged_out
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(user_logged_in)
+def set_fetch_coin_data_flag(sender, request, user, **kwargs):
+    user.userprofile.fetch_coin_data_flag = True
+    user.userprofile.save()
+
+@receiver(user_logged_out)
+def unset_fetch_coin_data_flag(sender, request, user, **kwargs):
+    user.userprofile.fetch_coin_data_flag = False
+    user.userprofile.save()
