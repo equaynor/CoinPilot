2eb71b4c2a3e323a780b716dae19a9809c32e732
equaynor <e.c.quaynor@gmail.com>
2024-05-03 15:23:59 +0000

    Add trade urls to coinpilot urls.py

--- coinpilot/urls.py
+++ coinpilot/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('portfolio/', include('portfolio.urls')),
+    path('trade/', include('trade.urls')),
 ]
