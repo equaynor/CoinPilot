15193c6fabb05170a6fc529cf615eae1e25265b6
equaynor <e.c.quaynor@gmail.com>
2024-05-03 15:24:44 +0000

    Add add trade view

--- trade/views.py
+++ trade/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Trade
+from .forms import TradeForm
 
-# Create your views here.
+@login_required
+def add_trade(request):
+    if request.method == 'POST':
+        form = TradeForm(request.POST)
+        if form.is_valid():
+            trade = form.save(commit=False)
+            trade.save()
+            return redirect('trade_history')
+    else:
+        form = TradeForm()
+    return render(request, 'trade/add_trade.html', {'form': form})
