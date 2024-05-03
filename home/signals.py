e3bd28e3ca47093f988fdd8d3a42973fd4dd61fb
equaynor <e.c.quaynor@gmail.com>
2024-05-03 10:11:43 +0000

    Call management command when flagging

--- home/signals.py
+++ home/signals.py
@@ -3,6 +3,7 @@ from allauth.account.signals import user_logged_in, user_logged_out
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from .models import UserProfile
+from django.core.management import call_command
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -13,6 +14,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 def set_fetch_coin_data_flag(sender, request, user, **kwargs):
     user.userprofile.fetch_coin_data_flag = True
     user.userprofile.save()
+    call_command('fetch_coin_data')
 
 @receiver(user_logged_out)
 def unset_fetch_coin_data_flag(sender, request, user, **kwargs):
