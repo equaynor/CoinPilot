0ae5f835807411dbdb5174a3a9e6012d232df1b4
equaynor <e.c.quaynor@gmail.com>
2024-05-06 10:17:21 +0000

    Add portfolio id to trade url paths

--- trade/urls.py
+++ trade/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add/', views.add_trade, name='add_trade'),
-    path('history/', views.trade_history, name='trade_history'),
+    path('add/<int:portfolio_id>/', views.add_trade, name='add_trade'),
+    path('history/<int:portfolio_id>/', views.trade_history, name='trade_history')
 ]
