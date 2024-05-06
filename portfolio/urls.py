64e42a3e90d46ec1a046c46fc57309074ef09e73
equaynor <e.c.quaynor@gmail.com>
2024-05-06 10:22:48 +0000

    Add portfolio url paths

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.portfolio_redirect, name='portfolio_redirect'),
+    path('<int:portfolio_id>/', views.portfolio_detail, name='portfolio_detail'),
     path('coins/', views.coin_list, name='coin_list'),
 ]
