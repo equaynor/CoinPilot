7461f5f0c7742f02b4791eac7307cdd36c2a6f8b
equaynor <e.c.quaynor@gmail.com>
2024-05-06 10:18:28 +0000

    Fix trade form by importing and defining coin

--- trade/forms.py
+++ trade/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 from .models import Trade
+from coin.models import Coin
 
 class TradeForm(forms.ModelForm):
+    coin = forms.ModelChoiceField(queryset=Coin.objects.all())
+
     class Meta:
         model = Trade
         fields = ['coin', 'trade_type', 'quantity', 'price']
