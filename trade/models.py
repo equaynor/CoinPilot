4d84f8005a2fc6d498587deb09a5bfe87c89e37c
equaynor <e.c.quaynor@gmail.com>
2024-05-06 10:16:55 +0000

    Add portfolio to trade model

--- trade/models.py
+++ trade/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from portfolio.models import Portfolio
 from holding.models import Holding
 from coin.models import Coin
 
@@ -7,12 +8,12 @@ class Trade(models.Model):
         ('BUY', 'Buy'),
         ('SELL', 'Sell'),
     )
-
-    holding = models.ForeignKey(Holding, on_delete=models.CASCADE, related_name='trades')
-    coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
-    trade_type = models.CharField(max_length=4, choices=TRADE_TYPE_CHOICES)
-    quantity = models.DecimalField(max_digits=18, decimal_places=8)
-    price = models.DecimalField(max_digits=18, decimal_places=2)
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, related_name='trades', null=True)
+    holding = models.ForeignKey(Holding, on_delete=models.CASCADE, related_name='trades', null=True)
+    coin = models.ForeignKey(Coin, on_delete=models.CASCADE, null=True)
+    trade_type = models.CharField(max_length=4, choices=TRADE_TYPE_CHOICES, default='BUY')
+    quantity = models.DecimalField(max_digits=18, decimal_places=8, default=0)
+    price = models.DecimalField(max_digits=18, decimal_places=2, default=0)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
