4300b14d98ce8aa2008b14e505f376e05e809a5e
equaynor <e.c.quaynor@gmail.com>
2024-05-08 11:19:11 +0000

    Remove holding from trade model

--- trade/models.py
+++ trade/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from portfolio.models import Portfolio
-from holding.models import Holding
 from coin.models import Coin
 
 class Trade(models.Model):
@@ -9,7 +8,6 @@ class Trade(models.Model):
         ('SELL', 'Sell'),
     )
     portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, related_name='trades', null=True)
-    holding = models.ForeignKey(Holding, on_delete=models.CASCADE, related_name='trades', null=True)
     coin = models.ForeignKey(Coin, on_delete=models.CASCADE, null=True)
     trade_type = models.CharField(max_length=4, choices=TRADE_TYPE_CHOICES, default='BUY')
     quantity = models.DecimalField(max_digits=18, decimal_places=8, default=0)
