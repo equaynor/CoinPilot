3b9e59b79398ffff3d53709443e76a8b7f655618
Emmanuel Quaynor <e.c.quaynor@gmail.com>
2024-05-24 11:40:19 +0000

    Fix delete trade url path

--- trade/urls.py
+++ trade/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import trade_delete
+
 
 urlpatterns = [
     path('add/<int:portfolio_id>/', views.add_trade, name='add_trade'),
     path('history/<int:portfolio_id>/', views.trade_history, name='trade_history'),
     path('trade/<int:trade_id>/edit/', views.edit_trade, name='edit_trade'),
-    path('delete/<int:trade_id>/', views.trade_delete, name='trade_delete'),
+    path('delete/<int:trade_id>/', views.delete_trade, name='delete_trade'),
 ]
