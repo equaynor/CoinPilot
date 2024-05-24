43e405c195fc8fe65f486c9949dcd51709e9d44f
Emmanuel Quaynor <e.c.quaynor@gmail.com>
2024-05-24 11:29:59 +0000

    Fix edit trade path

--- trade/urls.py
+++ trade/urls.py
@@ -5,6 +5,6 @@ from .views import trade_delete
 urlpatterns = [
     path('add/<int:portfolio_id>/', views.add_trade, name='add_trade'),
     path('history/<int:portfolio_id>/', views.trade_history, name='trade_history'),
-    path('trade/<int:trade_id>/edit/', views.trade_edit, name='trade_edit'),
+    path('trade/<int:trade_id>/edit/', views.edit_trade, name='edit_trade'),
     path('delete/<int:trade_id>/', views.trade_delete, name='trade_delete'),
 ]
