0bf845849e70d43b8bc9c03971d962c8ea56738f
Emmanuel Quaynor <e.c.quaynor@gmail.com>
2024-05-24 11:28:27 +0000

    Add holding field to trade object

--- trade/models.py
+++ trade/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from portfolio.models import Portfolio
+from holding.models import Holding
 from coin.models import Coin
 from django.utils import timezone
 
@@ -9,6 +10,7 @@ class Trade(models.Model):
         ('SELL', 'Sell'),
     )
     portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, related_name='trades', null=True)
+    holding = models.ForeignKey(Holding, on_delete=models.CASCADE, related_name='related_trades', null=True)
     coin = models.ForeignKey(Coin, on_delete=models.CASCADE, null=True)
     trade_type = models.CharField(max_length=4, choices=TRADE_TYPE_CHOICES, default='BUY')
     quantity = models.DecimalField(max_digits=18, decimal_places=8, default=0)
