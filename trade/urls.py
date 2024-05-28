c9ae039bfee5a556b50ab7db300be6a7698f21a1
Emmanuel Quaynor <e.c.quaynor@gmail.com>
2024-05-28 12:37:33 +0000

    Fix edit trade url path and change html name

--- trade/urls.py
+++ trade/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('add/<int:portfolio_id>/', views.add_trade, name='add_trade'),
     path('history/<int:portfolio_id>/', views.trade_history, name='trade_history'),
-    path('trade/<int:trade_id>/edit/', views.edit_trade, name='edit_trade'),
+    path('<int:portfolio_id>/<int:trade_id>/edit/', views.edit_trade, name='edit_trade'),
     path('delete/<int:trade_id>/', views.delete_trade, name='delete_trade'),
 ]
