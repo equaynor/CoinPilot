7054ed1449c43ab36f24086fcdbd067543e01c64
Emmanuel Quaynor <e.c.quaynor@gmail.com>
2024-06-09 01:37:58 +0000

    Delete delete portfolio js file

--- static/js/delete_portfolio.js
+++
@@ -1,4 +0,0 @@
-function confirmDeletion() {
-    const selectedPortfolio = document.getElementById('portfolio_dropdown').selectedOptions[0].text;
-    return confirm(`Are you sure you want to delete the portfolio: ${selectedPortfolio}?`);
-}
